4a28de9377da8d348ad5739a9b99a46829b88674
eKliman <kliman.evgeniy@gmail.com>
2020-05-07 21:45:20 +0300

    User authorization was added

--- js/main.js
+++ js/main.js
@@ -1,25 +1,109 @@
-const cartButton = document.querySelector('#cart-button');
+const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-
-// create function for toggle
-function toggleModal() {
-  modal.classList.toggle("is-open")
-}
 
 cartButton.addEventListener("click", toggleModal);
 close.addEventListener("click", toggleModal);
 
-new WOW().init();
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
 
-// VARIANT 2
+// day 1
 
-// cartButton.addEventListener("click", function (event) {
-//   modal.classList.add("is-open")
+// console.log(buttonAuth);
+// console.log(modalAuth);
+// console.dir(modalAuth);
+
+// buttonAuth.addEventListener("click", function () {
+//   console.log("Hello");
 // });
-// close.addEventListener("click", function (event) {
-//   modal.classList.remove("is-open")
-// });
 
+// buttonAuth.removeEventListener("click", toggleModalAuth);
 
-//console.log(cartButton); //display to console
+// loginInput.classList.contains("notEntered");
+
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
+
+let login = localStorage.getItem("gloDelivery");
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle("is-open");
+}
+function clearModalAuth() {
+  logInForm.reset();
+  loginInput.style.border = "";
+  loginInput.placeholder = "";
+  loginInput.classList.remove("notEntered");
+}
+
+function authorized() {
+  function logOut() {
+    login = null;
+    localStorage.removeItem("gloDelivery");
+    buttonAuth.style.display = "";
+    userName.style.display = "";
+    buttonOut.style.display = "";
+    buttonOut.removeEventListener("click", logOut);
+    checkAuth();
+  }
+  console.log("Авторизован");
+
+  userName.textContent = login;
+
+  buttonAuth.style.display = "none";
+  userName.style.display = "inline";
+  buttonOut.style.display = "block";
+
+  buttonOut.addEventListener("click", logOut);
+}
+
+function notAuthorized() {
+  console.log("Не авторизован");
+
+  function logIn(event) {
+    // console.log(event);
+    event.preventDefault();
+    login = loginInput.value;
+
+    if (login) {
+      localStorage.setItem("gloDelivery", login);
+
+      toggleModalAuth();
+      buttonAuth.removeEventListener("click", toggleModalAuth);
+      closeAuth.removeEventListener("click", toggleModalAuth);
+      logInForm.removeEventListener("submit", logIn);
+      logInForm.reset();
+      loginInput.style.border = "";
+      loginInput.placeholder = "";
+      loginInput.classList.remove("notEntered");
+      checkAuth();
+    } else {
+      loginInput.style.border = "2px solid #ff0000";
+      loginInput.placeholder = "Введите Логин";
+      loginInput.classList.add("notEntered");
+    }
+  }
+
+  buttonAuth.addEventListener("click", toggleModalAuth);
+  closeAuth.addEventListener("click", toggleModalAuth);
+  closeAuth.addEventListener("click", clearModalAuth);
+  logInForm.addEventListener("submit", logIn);
+}
+
+//If the login contains an empty string, it means it's False
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+
+checkAuth();
